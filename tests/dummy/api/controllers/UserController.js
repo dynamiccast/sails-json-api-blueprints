89dfdcee2eed2b628d6ac2217964df651892f831
Olivier Berthonneau <berthonneau.olivier@gmail.com>
2016-06-27 21:14:35 +0200

    Augment UserController to return fake user if me=true is queried

--- tests/dummy/api/controllers/UserController.js
+++ tests/dummy/api/controllers/UserController.js
@@ -5,7 +5,26 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-module.exports = {
-	
-};
+var findRecords = require('sails-json-api-blueprints/lib/api/blueprints/find');
 
+module.exports = {
+
+  find: function(req, res) {
+
+    if (req.allParams()["me"] === "true") {
+      return res.ok({
+        'data': {
+          'attributes': {
+            'email': 'root@jsonapi.com',
+            'first-name':'User',
+            'last-name':'Root'
+          },
+          'id': "0",
+          'type':'users'
+        }
+      });
+    }
+
+    return findRecords(req, res);
+  }
+};

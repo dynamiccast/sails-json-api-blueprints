0f0ba700b0e2bb6d58c26bde70a431ba700fd5da
Olivier Berthonneau <berthonneau.olivier@gmail.com>
2016-06-29 23:09:18 +0200

    Implement serialize function as a service for any custom actions to use
    JsonApiService is available at all time and expose a serialize method to convert any waterline data in JSON API valid format
    Test JsonApiService presence in CategoryController (totally arbitrary I know)

--- tests/dummy/api/controllers/CategoryController.js
+++ tests/dummy/api/controllers/CategoryController.js
@@ -11,8 +11,13 @@ module.exports = {
 
   create: function(req, res) {
 
+    if (typeof JsonApiService === "undefined") {
+      throw new Error("JsonApiService is not injected");
+    }
+
     req.body.data.attributes.name = req.body.data.attributes.name.trim();
 
     return createRecord(req, res);
   }
+
 };

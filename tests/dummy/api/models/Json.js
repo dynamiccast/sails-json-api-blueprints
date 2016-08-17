ae883507f97ca61e4d6e7c682ec12d9e05d9ad4a
Olivier Berthonneau <berthonneau.olivier@gmail.com>
2016-08-17 21:51:02 +0200

    Test toJSON is called prior to serialization

---
+++ tests/dummy/api/models/Json.js
@@ -0,0 +1,29 @@
+/**
+ * Json.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    variable: {
+      type: 'string'
+    },
+    invisible: {
+      type: 'string'
+    },
+
+    toJSON: function() {
+
+      var obj = this.toObject();
+      delete obj.invisible;
+
+      return obj;
+    }
+  },
+
+  autoCreatedAt: false,
+  autoUpdatedAt: false
+};
